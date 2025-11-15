6dd6dc085421944b9a309af802da8b103b0c58eb
dinkerpaul138 <dinkerpaul138@gmail.com>
2025-11-15 11:47:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+class Animal {
+    public void eat() {
+        System.out.println("This animal is eating.");
+    }
+}
+class Dog extends Animal {
+    public void bark() {
+        System.out.println("The dog is barking!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.bark();  // Output: The dog is barking!
+    }
+}
